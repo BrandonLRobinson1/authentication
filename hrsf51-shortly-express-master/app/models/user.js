5ab93601575966c078fe867b6ead6b03e4b832f4
BrandonLRobinson1 <BrandonLRobinson1@gmail.com>
2017-03-17 10:06:46 -0700

    updating user

--- hrsf51-shortly-express-master/app/models/user.js
+++ hrsf51-shortly-express-master/app/models/user.js
@@ -6,12 +6,12 @@ var Promise = require('bluebird');
 
 var User = db.Model.extend({
   tableName: 'users',
-  hasTimestamps: true,
-  setPassword: function(unencryptedPassword) {
-    bcrypt.hash(unencryptedPassword, null, null, (function(err, hash) {
-      this.save('password', hash);
-    }).bind(this));
-  }
+  hasTimestamps: true
+  // setPassword: function(unencryptedPassword) {
+  //   bcrypt.hash(unencryptedPassword, null, null, (function(err, hash) {
+  //     this.save('password', hash);
+  //   }).bind(this));
+  // }
 });
 
 module.exports = User;
